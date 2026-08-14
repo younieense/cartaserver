d19994cb20d68d5c6ad82fa0759bd024bbb41fe4
younieense <younieense@gmail.com>
2026-08-14 21:02:30 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")"
-if [[ ! -d .venv ]]; then
-  python3 -m venv .venv
-  .venv/bin/pip install -r requirements.txt
-fi
-exec .venv/bin/python -m app.main
